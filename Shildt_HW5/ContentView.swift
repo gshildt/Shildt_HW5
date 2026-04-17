8e90aed54e7284659ec540773c36d0307d9ec772
Greg Shildt <gshildt@okstate.edu>
2026-04-17 11:32:32 -0500

    Initial Commit

---
+++ Shildt_HW5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shildt_HW5
+//
+//  Created by Greg Shildt on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shildt_HW5/Shildt_HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  Shildt_HW5App.swift
+//  Shildt_HW5
+//
+//  Created by Greg Shildt on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Shildt_HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
